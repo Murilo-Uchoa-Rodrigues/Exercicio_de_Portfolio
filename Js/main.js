05cdcbf0db2308c74ee195e26fadac7169b5a9a5
Murilo-Uchoa-Rodrigues <murilouchoarodrigues.oficial@gmail.com>
2023-04-05 20:44:33 -0300

    Exercício de Portfólio v3

--- Js/main.js
+++ Js/main.js
@@ -1,8 +1,20 @@
 var btnContact = document.querySelector('.jl-btn-contact');
 
+window.addEventListener('load', function () {
+    var pagePreloder = document.querySelector('.jl-preloader');
+    pagePreloder.classList.add('jl-fade-out');
+
+
+    setTimeout(function () {
+        pagePreloder.style.display = 'none';
+    }, 2000);
+
+
+});
+
 
 btnContact.addEventListener('click', function () {
     var boxContact = document.querySelector('.jl-contact-info');
-    boxContact.classList.toggle('jl-is-open')
-    this.classList.toggle('jl-change-icon')
+    boxContact.classList.toggle('jl-is-open');
+    this.classList.toggle('jl-change-icon');
 });
